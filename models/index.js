e5f86cedd53c7b85abc624e4f9c9b6a241f99fdd
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-14 18:28:09 -0500

    API routingd

--- models/index.js
+++ models/index.js
@@ -8,12 +8,17 @@ const ProductTag = require("./ProductTag");
 Product.belongsTo(Category, { foreignKey: "category_id" });
 
 // Categories have many Products //
-Category.hasMany(Product, { foreignKey: "category_id" });
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag) //
+// mini project syntax vs below comment?
 Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
 
 // Tags belongToMany Products (through ProductTag) //
+// this syntax vs index.js in mini project??
 Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id" });
 
 module.exports = {
